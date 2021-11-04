0896a90a900bddd3f73722ed9d462498f99643c9
Yarolf <yarafirst18@gmail.com>
2021-11-04 19:53:52 +0800

    delete disabling loggers

--- telegram/bot.py
+++ telegram/bot.py
@@ -4,10 +4,3 @@ from config import TOKEN
 
 bot = Bot(token=TOKEN)
 dispatcher = Dispatcher(bot)
-
-# disabling other loggers
-import logging.config
-logging.config.dictConfig({
-    'version': 1,
-    'disable_existing_loggers': True,
-})

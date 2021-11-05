7303fdf58ad54ed61bc6380f9787bafecd63b1c9
Yarolf <yarafirst18@gmail.com>
2021-11-05 19:49:05 +0800

    refactor method names

--- telegram/handlers/user.py
+++ telegram/handlers/user.py
@@ -21,7 +21,7 @@ async def process_start_command(message: types.Message):
 
 @dp.message_handler(commands=['test'])
 async def process_test_command(message: types.Message):
-    question: QuestionBlock = QuestionBlock.get(QuestionBlock.tour_number == 1)
+    question: QuestionBlock = QuestionBlock.get(1)
     try:
         possible_answers = PossibleAnswer.select().execute()
         await question.send_to_user(message, possible_answers, USER_ANSWER_PREFIX)
@@ -35,13 +35,13 @@ async def process_answer_call(call_back: CallbackQuery):
     await call_back.message.delete()
     call_back_data = call_back.data.lstrip(USER_ANSWER_PREFIX.get_full_prefix())
     split_data = call_back_data.split(USER_ANSWER_PREFIX.split_character)
-    tour_number = int(split_data[0])
+    question_number = int(split_data[0])
     answer_id = split_data[1]
-    Answer.insert(user=call_back.from_user.id, answer=answer_id).execute()
+    Answer.insert(user=call_back.from_user.id, question=question_number, answer=answer_id).execute()
 
     try:
-        question = get_next_tour(tour_number)
-    except OutOfTours:
+        question = get_next_question(question_number)
+    except OutOfQuestions:
         await call_back.message.answer('Спасибо за участие!')
         await call_back.answer()
         return
@@ -55,14 +55,14 @@ async def process_answer_call(call_back: CallbackQuery):
     await call_back.answer()
 
 
-def get_next_tour(tour_number):
+def get_next_question(tour_number):
     next_tour_number = tour_number + 1
     try:
-        return QuestionBlock.get(QuestionBlock.tour_number == next_tour_number)
+        return QuestionBlock.get(next_tour_number)
     except peewee.DoesNotExist:
-        raise OutOfTours('Туров больше не осталось!')
+        raise OutOfQuestions('Вопросов больше не осталось!')
 
 
-class OutOfTours(Exception):
+class OutOfQuestions(Exception):
     pass
 

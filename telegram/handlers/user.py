e74d57f0bc7ba73ed90d007f07b9f2f5022e5680
Yarolf <yarafirst18@gmail.com>
2021-11-05 17:16:17 +0800

    temporary fix

--- telegram/handlers/user.py
+++ telegram/handlers/user.py
@@ -30,14 +30,18 @@ async def process_test_command(message: types.Message):
 
 @dp.callback_query_handler(lambda call: call.data.startswith(USER_ANSWER_PREFIX.prefix))
 async def process_answer_call(call_back: CallbackQuery):
-    await call_back.message.delete()
-    call_back_data = call_back.data.lstrip(USER_ANSWER_PREFIX.get_full_prefix())
-    tour_number = int(call_back_data.split(USER_ANSWER_PREFIX.split_character)[0]) + 1
-    print(tour_number)
-    question: QuestionBlock = QuestionBlock.get(QuestionBlock.tour_number == tour_number)
     try:
-        possible_answers = PossibleAnswer.select().execute()
-        await question.send_to_user(call_back.message, possible_answers, USER_ANSWER_PREFIX)
-    except AttachmentNotSpecifiedError:
-        await call_back.message.answer('Что-то пошло не так(')
-        logging.info(f'Тип файла не поддерживается: {question.file_name}')
+        await call_back.message.delete()
+        call_back_data = call_back.data.lstrip(USER_ANSWER_PREFIX.get_full_prefix())
+        tour_number = int(call_back_data.split(USER_ANSWER_PREFIX.split_character)[0]) + 1
+        print(tour_number)
+        question: QuestionBlock = QuestionBlock.get(QuestionBlock.tour_number == tour_number)
+        try:
+            possible_answers = PossibleAnswer.select().execute()
+            await question.send_to_user(call_back.message, possible_answers, USER_ANSWER_PREFIX)
+        except AttachmentNotSpecifiedError:
+            await call_back.message.answer('Что-то пошло не так(')
+            logging.info(f'Тип файла не поддерживается: {question.file_name}')
+    except Exception:
+        await call_back.message.answer('Спасибо за участие!')
+

8be38559a14839ab3162ce236fa2042190248e66
Yarolf <yarafirst18@gmail.com>
2021-11-09 22:12:32 +0800

    replace registrator to class

--- telegram/handlers/registrator.py
+++ telegram/handlers/registrator.py
@@ -10,38 +10,39 @@ from .message_handlers.registration_handler import register, Registration
 from telegram.bot import dispatcher as dp
 
 
-def register_all_handlers():
-    register_error_handlers()
-    register_base_commands_handlers()
-    register_user_registration_handlers()
-    register_test_handlers()
+class HandlerRegistrar:
+    def register_all_handlers(self):
+        self.register_error_handlers()
+        self.register_base_commands_handlers()
+        self.register_user_registration_handlers()
+        self.register_test_handlers()
 
+    @staticmethod
+    def register_error_handlers():
+        dp.register_errors_handler(handle_error)
 
-def register_error_handlers():
-    dp.register_errors_handler(handle_error)
+    @staticmethod
+    def register_base_commands_handlers():
+        dp.register_message_handler(process_start_command, commands=['start'])
+        dp.register_message_handler(process_help_command, commands=['help'])
+        dp.register_message_handler(process_about_command, commands=['about'])
 
+    @classmethod
+    def register_user_registration_handlers(cls):
+        dp.register_message_handler(process_register_command, commands=['register'])
+        dp.register_message_handler(request_fio, commands=['update_info'])
+        dp.register_message_handler(process_cancel_command, state=Registration.fio, commands=['cancel'])
+        dp.register_message_handler(handle_commands_during_registration,
+                                    cls.__all_commands_except_cancel,
+                                    state=Registration.fio)
+        dp.register_message_handler(register, state=Registration.fio)
 
-def register_base_commands_handlers():
-    dp.register_message_handler(process_start_command, commands=['start'])
-    dp.register_message_handler(process_help_command, commands=['help'])
-    dp.register_message_handler(process_about_command, commands=['about'])
+    @staticmethod
+    def __all_commands_except_cancel(message: types.Message):
+        return message.text.startswith('/') and not message.text == '/cancel'
 
-
-def register_user_registration_handlers():
-    dp.register_message_handler(process_register_command, commands=['register'])
-    dp.register_message_handler(request_fio, commands=['update_info'])
-    dp.register_message_handler(process_cancel_command, state=Registration.fio, commands=['cancel'])
-    dp.register_message_handler(handle_commands_during_registration,
-                                __all_commands_except_cancel,
-                                state=Registration.fio)
-    dp.register_message_handler(register, state=Registration.fio)
-
-
-def __all_commands_except_cancel(message: types.Message):
-    return message.text.startswith('/') and not message.text == '/cancel'
-
-
-def register_test_handlers():
-    dp.register_message_handler(process_test_command, commands=['test'])
-    dp.register_callback_query_handler(process_answer_call,
-                                       lambda call: call.data.startswith(USER_ANSWER_PREFIX.prefix))
+    @staticmethod
+    def register_test_handlers():
+        dp.register_message_handler(process_test_command, commands=['test'])
+        dp.register_callback_query_handler(process_answer_call,
+                                           lambda call: call.data.startswith(USER_ANSWER_PREFIX.prefix))
